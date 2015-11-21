b4081788c02e3160b979a0362087f9e24691b12c
A. Rosenberg Johansen <alexander@herhjemme.dk>
2015-11-21 17:01:13 +0800

    test

--- mnist_test.py
+++ mnist_test.py
@@ -13,14 +13,13 @@ from skimage import transform
 import matplotlib.pyplot as plt
 
 import lasagne
+import string
 
-
-# ################## Download and prepare the MNIST dataset ##################
-# This is just some way of getting the MNIST dataset from an online location
-# and loading it into numpy arrays.
+# ########################### Getting the data #############################
+# Downloads and modifies the MNIST to be a noise grid of twice it's size 
+# with Mnist numbers randomly placed on the grid
 
 def load_dataset():
-    # We first define a download function, supporting both Python 2 and 3.
     if sys.version_info[0] == 2:
         from urllib import urlretrieve
     else:
@@ -29,19 +28,13 @@ def load_dataset():
     def download(filename, source='http://yann.lecun.com/exdb/mnist/'):
         print("Downloading %s" % filename)
         urlretrieve(source + filename, filename)
-
-    # We then define functions for loading MNIST images and labels.
-    # For convenience, they also download the requested files if needed.
     import gzip
 
     def load_mnist_images(filename):
         if not os.path.exists(filename):
             download(filename)
-        # Read the inputs in Yann LeCun's binary format.
         with gzip.open(filename, 'rb') as f:
             data = np.frombuffer(f.read(), np.uint8, offset=16)
-        # The inputs are vectors now, we reshape them to monochrome 2D images,
-        # following the shape convention: (examples, channels, rows, columns)
         data = data.reshape(-1, 1, 28, 28)
         #############
         # @@@@@ Making the scaled version for Zooming-SPN @@@@@
@@ -66,54 +59,43 @@ def load_dataset():
         print("shapes of data_orig and data_rescaled")        
         print(data_orig.shape)
         print(data_rescaled.shape)
-        print("saving images ...")
+        print("saving images of the grid and rescaled grid ...")
         plt.imsave(fname="orig", arr=data_orig[100,0,:,:], cmap=plt.cm.gray)
         plt.imsave(fname="rescaled", arr=data_rescaled[100,0,:,:], cmap=plt.cm.gray)
         print("images saved!")
-        # The inputs come as bytes, we convert them to float32 in range [0,1].
-        # (Actually to range [0, 255/256], for compatibility to the version
-        # provided at http://deeplearning.net/data/mnist/mnist.pkl.gz.)
         return data_orig, data_rescaled
 
     def load_mnist_labels(filename):
         if not os.path.exists(filename):
             download(filename)
-        # Read the labels in Yann LeCun's binary format.
         with gzip.open(filename, 'rb') as f:
             data = np.frombuffer(f.read(), np.uint8, offset=8)
-        # The labels are vectors of integers now, that's exactly what we want.
         return data
 
-    # We can now download and read the training and test set images and labels.
     X_train_orig, X_train_rescaled = load_mnist_images('train-images-idx3-ubyte.gz')
     y_train = load_mnist_labels('train-labels-idx1-ubyte.gz')
     X_test_orig, X_test_rescaled = load_mnist_images('t10k-images-idx3-ubyte.gz')
     y_test = load_mnist_labels('t10k-labels-idx1-ubyte.gz')
 
-    # We reserve the last 10000 training examples for validation.
     X_train_orig, X_val_orig = X_train_orig[:-10000], X_train_orig[-10000:]
     X_train_rescaled, X_val_rescaled = X_train_rescaled[:-10000], X_train_rescaled[-10000:]
     y_train, y_val = y_train[:-10000], y_train[-10000:]
 
-    # We just return all the arrays in order, as expected in main().
-    # (It doesn't matter how we do this as long as we can read them again.)
     return X_train_orig, X_train_rescaled, y_train, X_val_orig, X_val_rescaled, y_val, X_test_orig, X_test_rescaled, y_test
 
 
 # ##################### Build the neural network model #######################
-# This script supports three types of models. For each one, we define a
-# function that takes a Theano variable representing the input and returns
-# the output layer of a neural network model built in Lasagne.
+# Builds an spn with option of zooming on the original image
 
 
-def build_zoom_spn(input_var_orig=None, input_var_rescaled=None):
+def build_spn_cnn(input_var_orig=None, input_var_rescaled=None, zoom=True):
     # Input images
     l_in_orig = lasagne.layers.InputLayer(shape=(None, 1, 28*2, 28*2),
                                         input_var=input_var_orig)
 
     l_in_rescaled = lasagne.layers.InputLayer(shape=(None, 1, 28, 28),
                                         input_var=input_var_rescaled)
-    # conv for the SPN
+    # CNN for the SPN
     l_conv_spn_1_a = lasagne.layers.Conv2DLayer(
             l_in_rescaled, num_filters=16, filter_size=(3, 3))
     l_conv_spn_1_b = lasagne.layers.Conv2DLayer(
@@ -121,14 +103,17 @@ def build_zoom_spn(input_var_orig=None, input_var_rescaled=None):
     l_mp_spn_1 = lasagne.layers.MaxPool2DLayer(l_conv_spn_1_b, pool_size=(2, 2))
     l_dense_spn_1 = lasagne.layers.DenseLayer(
             lasagne.layers.dropout(l_mp_spn_1, p=.5), num_units=128)
-    b = np.zeros((2,3), dtype='float32')
+    b = np.zeros((2, 3), dtype='float32')
     b[0, 0] = 1
     b[1, 1] = 1
     b = b.flatten()
     W = lasagne.init.Constant(0.0)
     l_dense_spn_out = lasagne.layers.DenseLayer(
             l_dense_spn_1, num_units=6, W=W, b=b)
-    l_spn_1 = lasagne.layers.TransformerLayer(l_in_rescaled, l_dense_spn_out, downsample_factor=2)
+    if zoom:
+        l_spn_1 = lasagne.layers.TransformerLayer(l_in_orig, l_dense_spn_out, downsample_factor=2)
+    else:
+        l_spn_1 = lasagne.layers.TransformerLayer(l_in_rescaled, l_dense_spn_out, downsample_factor=2)
     l_conv_1_a = lasagne.layers.Conv2DLayer(
             l_spn_1, num_filters=16, filter_size=(3, 3))
     l_conv_1_b = lasagne.layers.Conv2DLayer(
@@ -142,15 +127,44 @@ def build_zoom_spn(input_var_orig=None, input_var_rescaled=None):
             nonlinearity=lasagne.nonlinearities.softmax)
     return l_out
 
+def build_spn_cnn_large(input_var_orig=None, input_var_rescaled=None, zoom=True):
+    # Input images
+    l_in_orig = lasagne.layers.InputLayer(shape=(None, 1, 28*2, 28*2),
+                                        input_var=input_var_orig)
 
-# ############################# Batch iterator ###############################
-# This is just a simple helper function iterating over training data in
-# mini-batches of a particular size, optionally in random order. It assumes
-# data is available as numpy arrays. For big datasets, you could load numpy
-# arrays as memory-mapped files (np.load(..., mmap_mode='r')), or write your
-# own custom data iteration function. For small datasets, you can also copy
-# them to GPU at once for slightly improved performance. This would involve
-# several changes in the main program, though, and is not demonstrated here.
+    l_in_rescaled = lasagne.layers.InputLayer(shape=(None, 1, 28, 28),
+                                        input_var=input_var_rescaled)
+    # CNN for the SPN
+    l_conv_spn_1_a = lasagne.layers.Conv2DLayer(
+            l_in_rescaled, num_filters=32, filter_size=(3, 3))
+    l_conv_spn_1_b = lasagne.layers.Conv2DLayer(
+            l_conv_spn_1_a, num_filters=32, filter_size=(3, 3))
+    l_mp_spn_1 = lasagne.layers.MaxPool2DLayer(l_conv_spn_1_b, pool_size=(2, 2))
+    l_dense_spn_1 = lasagne.layers.DenseLayer(
+            lasagne.layers.dropout(l_mp_spn_1, p=.5), num_units=256)
+    b = np.zeros((2, 3), dtype='float32')
+    b[0, 0] = 1
+    b[1, 1] = 1
+    b = b.flatten()
+    W = lasagne.init.Constant(0.0)
+    l_dense_spn_out = lasagne.layers.DenseLayer(
+            l_dense_spn_1, num_units=6, W=W, b=b)
+    if zoom:
+        l_spn_1 = lasagne.layers.TransformerLayer(l_in_orig, l_dense_spn_out, downsample_factor=2)
+    else:
+        l_spn_1 = lasagne.layers.TransformerLayer(l_in_rescaled, l_dense_spn_out, downsample_factor=2)
+    l_conv_1_a = lasagne.layers.Conv2DLayer(
+            l_spn_1, num_filters=32, filter_size=(3, 3))
+    l_conv_1_b = lasagne.layers.Conv2DLayer(
+            l_conv_1_a, num_filters=64, filter_size=(3, 3))
+    l_mp_1 = lasagne.layers.MaxPool2DLayer(l_conv_1_b, pool_size=(2, 2))
+    l_dense_1 = lasagne.layers.DenseLayer(
+            lasagne.layers.dropout(l_mp_1, p=.5), num_units=256)
+    l_out = lasagne.layers.DenseLayer(
+            lasagne.layers.dropout(l_dense_1, p=.5),
+            num_units=10,
+            nonlinearity=lasagne.nonlinearities.softmax)
+    return l_out
 
 def iterate_minibatches(inputs_orig, inputs_rescaled, targets, batchsize, shuffle=False):
     assert len(inputs_orig) == len(targets)
@@ -171,28 +185,33 @@ def iterate_minibatches(inputs_orig, inputs_rescaled, targets, batchsize, shuffl
 # more functions to better separate the code, but it wouldn't make it any
 # easier to read.
 
-def main(model='zoom_spn', num_epochs=500):
-    # Load the dataset
+def main(model='spn_cnn', zoom=True, num_epochs=500):
     print("Loading data...")
     X_train_orig, X_train_rescaled, y_train, X_val_orig, X_val_rescaled, y_val, X_test_orig, X_test_rescaled, y_test = load_dataset()
 
-    # Prepare Theano variables for inputs and targets
     input_var_orig = T.tensor4('inputs_orig')
     input_var_rescaled = T.tensor4('inputs_rescaled')
     target_var = T.ivector('targets')
 
-    # Create neural network model (depending on first command line parameter)
     print("Building model and compiling functions...")
 
-    if model == 'zoom_spn':
-        network = build_zoom_spn(input_var_orig, input_var_rescaled)
+    if model == 'spn_cnn':
+        l_out = build_spn_cnn(input_var_orig, input_var_rescaled, zoom)
+    elif model == 'spn_cnn_large':
+        l_out = build_spn_cnn_large(input_var_orig, input_var_rescaled, zoom)     
     else:
         print("Unrecognized model type %r." % model)
         return
-
+    all_layers = lasagne.layers.get_all_layers(l_out)
+    num_params = lasagne.layers.count_params(l_out)
+    print("  number of parameters: %d" % num_params)
+    print("  layer output shapes:")
+    for layer in all_layers:
+        name = string.ljust(layer.__class__.__name__, 32)
+        print("    %s %s" % (name, lasagne.layers.get_output_shape(layer)))
     # Create a loss expression for training, i.e., a scalar objective we want
     # to minimize (for our multi-class problem, it is the cross-entropy loss):
-    prediction = lasagne.layers.get_output(network)
+    prediction = lasagne.layers.get_output(l_out)
     loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
     loss = loss.mean()
     # We could add some weight decay as well here, see lasagne.regularization.
@@ -200,14 +219,14 @@ def main(model='zoom_spn', num_epochs=500):
     # Create update expressions for training, i.e., how to modify the
     # parameters at each training step. Here, we'll use Stochastic Gradient
     # Descent (SGD) with Nesterov momentum, but Lasagne offers plenty more.
-    params = lasagne.layers.get_all_params(network, trainable=True)
+    params = lasagne.layers.get_all_params(l_out, trainable=True)
     updates = lasagne.updates.nesterov_momentum(
             loss, params, learning_rate=0.01, momentum=0.9)
 
     # Create a loss expression for validation/testing. The crucial difference
     # here is that we do a deterministic forward pass through the network,
     # disabling dropout layers.
-    test_prediction = lasagne.layers.get_output(network, deterministic=True)
+    test_prediction = lasagne.layers.get_output(l_out, deterministic=True)
     test_loss = lasagne.objectives.categorical_crossentropy(test_prediction,
                                                             target_var)
     test_loss = test_loss.mean()
@@ -283,16 +302,17 @@ if __name__ == '__main__':
         print("Trains a neural network on MNIST using Lasagne.")
         print("Usage: %s [MODEL [EPOCHS]]" % sys.argv[0])
         print()
-        print("MODEL: 'mlp' for a simple Multi-Layer Perceptron (MLP),")
-        print("       'custom_mlp:DEPTH,WIDTH,DROP_IN,DROP_HID' for an MLP")
-        print("       with DEPTH hidden layers of WIDTH units, DROP_IN")
-        print("       input dropout and DROP_HID hidden dropout,")
+        print("MODEL: 'spn_cnn' for a small spn_cnn")
+        print("       'spn_large' for a large spn_cnn")
         print("       'cnn' for a simple Convolutional Neural Network (CNN).")
+        print("ZOOM:  '1' for a zoom_spn_cnn or '0' for std. spn_cnn")
         print("EPOCHS: number of training epochs to perform (default: 500)")
     else:
         kwargs = {}
         if len(sys.argv) > 1:
             kwargs['model'] = sys.argv[1]
         if len(sys.argv) > 2:
-            kwargs['num_epochs'] = int(sys.argv[2])
+            kwargs['zoom'] = bool(int(sys.argv[2]))
+        if len(sys.argv) > 3:
+            kwargs['num_epochs'] = int(sys.argv[3])
         main(**kwargs)
